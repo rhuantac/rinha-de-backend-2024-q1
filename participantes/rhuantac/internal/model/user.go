f4322ef35ae88ae31c232c3d8a4fe6c8eb3f03ca
Rhuan Chaves <rhuan.chaves@vollsolutions.com.br>
2024-02-18 22:35:39 -0300

    Added docker compose and move files to other repo

--- participantes/rhuantac/internal/model/user.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type User struct {
-	ID             int `bson:"_id"`
-	Limit          int `bson:"limit"`
-	InitialBalance int `bson:"initial_balance"`
-	CurrentBalance int `bson:"current_balance"`
-}
